0b79d941e0dd287955a58f54ae0785508dedf5dc
Muhib Ullah <muhibullah0603@gmail.com>
2023-12-31 11:58:44 +0500

    frontend updated

--- frontend/src/pages/HomePage/Homepage2.jsx
+++ frontend/src/pages/HomePage/Homepage2.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import NavBar from '../../components/Navbar/Navbar'
 import LandingScreenVideo from '../assets/LandingScreen_video.mp4'
-import CollagePic1 from '../assets/main_collage-pic.jpg'
-import CollagePic2 from '../assets/steering_pic.jpg'
-import CollagePic3 from '../assets/brake_caliper.jpg'
+import CollagePic1 from '../assets/about_us-1.jpg'
+import CollagePic2 from '../assets/about_us-2.jpg'
+import CollagePic3 from '../assets/about_us-3.jpg'
 import './Homepage2.css'
+
 
 const Homepage2 = () => {
   return (
     <div className='main-container'>
-      <NavBar />
+      <NavBar className='navbar_landingScreen'/>
       {/* LANDING SCREEN */}
       <section className="Landing_screen-hero-container">
         <div className='Landing_screen-main-container'>
-          <video className="Landing_screen-BackgroundVideo" autoPlay muted>
+          <video className="Landing_screen-BackgroundVideo" autoPlay muted preload='auto'>
             <source src={LandingScreenVideo} type="video/mp4" />
           </video>
           <div className="Landing-screen-content-container">
@@ -31,46 +32,38 @@ const Homepage2 = () => {
             </div>
             <div className="copyright">
               <p>Content from ExoticaRides LLC, including its affiliates, is for general information only. None of the provided information is intended as investment advice. Use the company's website information at your own risk; the company assumes no responsibility or liability for any use or misuse of such information.</p>
-              <p style={{color:'white'}}>&copy; 2023 ExoticaRides LLC | All rights reserved.</p>
+              <p style={{ color: 'white' }}>&copy; 2023 ExoticaRides LLC | All rights reserved.</p>
             </div>
           </div>
         </div>
       </section>
       {/* ABOUT US SCREEN */}
       <section>
-        <div className="About_Us-main-container" style={{ marginTop:'50px' }}>
+        <div className="About_Us-main-container" style={{ marginTop: '50px' }}>
           <div className="container-fluid">
             <div className="each_section-top-heading-container">
-              <h1>ABOUT <span style={{color: '#05c1ae'}}>US</span></h1>
+              <h1>ABOUT <span style={{ color: '#05c1ae' }}>US</span></h1>
             </div>
             <div className="row">
-              <div className="col-lg-6 About_us-section-left-container">
-                <p>Step into ExoticaRides, where we blend luxury with your drive in the coolest way imaginable. Our remarkable fleet showcases a mix of chic sports cars, robust SUVs, and sophisticated sedans that don't just transport you from one place to another but transform every journey into an exceptional adventure. At ExoticaRides, we're not just about car rentals; we're here to curate an experience that stays with you.
-                  <br />
-                  Choosing a car with us goes beyond merely selecting a vehicle; it's about crafting memories that linger. From handpicking your dream car to hitting the road with a grin, ExoticaRides is your partner in turning driving dreams into a tangible reality. We don't just rent cars; we create moments that are unique and exhilarating.
-                  <br />
-                  What distinguishes ExoticaRides is our unwavering commitment to ensuring every facet of your experience is nothing short of amazing. We are dedicated to offering not just a car but an entire atmosphere that radiates coolness and excitement. Come join us at ExoticaRides, where each drive is an invitation to encounter something extraordinary, where luxury meets the road, and let's reshape how you perceive and relish car rentals. So, fasten your seatbelt and prepare for a ride unlike any other!</p>
-              </div>
-              <div className="col-lg-6 About_us-section-right-container"
-               style={{display:'flex', justifyContent:'center', alignItems:'center', gap:'15px'}}>
-                <div className="lamba_div">
-                  <img src={CollagePic1} alt="" style={{width:'100%', height:'500px'}}/>
-                </div>
-                <div className="chote_wale-div-container">
-                  <div className="choti_image">
-                    <img src={CollagePic2} alt=""  style={{width:'100%', height:'200px', marginBottom:'10px'}}/>
-                  </div>
-                  <div className="bari_ima" style={{alignItems:'center', display:'flex', justifyContent:'center'}}>
-                    <img src={CollagePic3} alt=""  style={{width:'95%', height:'350px'}} />
+              <div className="col-lg-12">
+                <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                  <div class="carousel-inner">
+                    <div class="carousel-item active" style={{height:'500px', overflow:"hidden"}}>
+                      <img src={CollagePic1} class="d-block w-100" alt="..." style={{width:'100%', height:'100%'}}/>
+                    </div>
+                    <div class="carousel-item">
+                      <img src={CollagePic2} class="d-block w-100" alt="..."/>
+                    </div>
+                    <div class="carousel-item">
+                      <img src={CollagePic3} class="d-block w-100" alt="..."/>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-
       </section>
-
     </div>
   );
 };
--- frontend/src/pages/ViewCarsPage/ViewCars.jsx
+++ frontend/src/pages/ViewCarsPage/ViewCars.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
-import Navbar  from '../../components/Navbar/Navbar'
+import Navbar from '../../components/Navbar/Navbar'
+import './ViewCars.css'
+import viewCarBG from '../assets/viewCar.jpg'
+import { useState } from 'react';
+
 const ViewCars = () => {
+  let [a, setA] = useState('Initial Value');
+  const getValue = (element) => {
+    a = element.value;
+    setA(a);
+  };
+
   return (
     <div>
-      <Navbar/>  
-      ViewCars
+      <div className="main_head-container">
+        <div className="background-image-container">
+          <img src={viewCarBG} alt="Background" />
+        </div>
+        <Navbar />
+        <div className="heading_content-container">
+          <h1>Find Your <span style={{ color: '#05c1ae' }}>Drive</span>.</h1>
+        </div>
+      </div>
+      <div className="container-fluid">
+        <div>
+          <input type="range" id="volume" name="volume" min="2000" max="2023" onChange={() => console.log(getValue(document.getElementById("volume")))}/>
+          <label htmlFor="volume">Volume</label>
+          <p>{a}</p>
+        </div>
+      </div>
     </div>
   )
 }
